895d3dd227444958b3e5f6582cb2297d3e6192d3
subrotoice <subroto.iu@gmail.com>
2024-04-22 06:58:57 +0600

    Adding a Confirmation Dialog Box

--- README.md
+++ README.md
@@ -1609,10 +1609,36 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 </main>;
 ```
 
-### -
+### - Adding a Confirmation Dialog Box
 
 ```jsx
-
+// issues/[id]/DeleteissueButton.tsx
+return (
+  <AlertDialog.Root>
+    <AlertDialog.Trigger>
+      <Button color='red'>Delete Issue</Button>
+    </AlertDialog.Trigger>
+    <AlertDialog.Content>
+      <AlertDialog.Title>Delete Confirmation</AlertDialog.Title>
+      <AlertDialog.Description>
+        Are you sure you want to delete this issue? This action can not be
+        undone.
+      </AlertDialog.Description>
+      <Flex gap='3' mt='4'>
+        <AlertDialog.Cancel>
+          <Button variant='soft' color='gray'>
+            Cancel
+          </Button>
+        </AlertDialog.Cancel>
+        <AlertDialog.Action>
+          <Button variant='solid' color='red'>
+            Delete Issue
+          </Button>
+        </AlertDialog.Action>
+      </Flex>
+    </AlertDialog.Content>
+  </AlertDialog.Root>
+);
 ```
 
 ### -
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,33 @@
-import { Button } from "@radix-ui/themes";
+import { Button, AlertDialog, Flex } from "@radix-ui/themes";
 import React from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color='red'>Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Confirmation</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action can not be
+          undone.
+        </AlertDialog.Description>
+        <Flex gap='3' mt='4'>
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'>
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant='solid' color='red'>
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
