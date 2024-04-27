20ce1ca6122c3a8fd43bedcba131b22906bce531
subrotoice <subroto.iu@gmail.com>
2024-04-27 21:06:29 +0600

    Populating the Assignee Select Component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = async () => {
+  const res = await fetch("http://localhost:3000/api/users");
+  const users: User[] = await res.json();
+
   return (
     <Select.Root>
       <Select.Trigger placeholder='Assign...' />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestion</Select.Label>
-          <Select.Item value='1'>Subroto Biswas</Select.Item>
+          {users.map((user) => (
+            <Select.Item value={user.id}>{user.name}</Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
