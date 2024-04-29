739cb9ad6ff8b41a18417731136b7ee43ada8aec
subrotoice <subroto.iu@gmail.com>
2024-04-29 17:21:27 +0600

    Showing Toast Notifications for error/success

--- README.md
+++ README.md
@@ -2598,10 +2598,62 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 };
 ```
 
-### -
+### - Showing Toast Notifications for error/success
+
+- [react-hot-toast.com](https://react-hot-toast.com) [see](https://prnt.sc/-93jhPED48Ia)
+
+```bash
+npm install react-hot-toast
+```
 
 ```jsx
+//
+"use client";
+import toast, { Toaster } from "react-hot-toast"; // added 1
 
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || "unassigned"}
+        onValueChange={(userId) => {
+          const valueUserId = userId === "unassigned" ? null : userId;
+          fetch("/api/issues/" + issue.id, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              assignedToUserId: valueUserId,
+            }),
+          })
+            .then((res) => {
+              if (!res.ok) {
+                // if you use axios then you need to to do this only .catch() work directly
+                throw new Error("Network response was not ok");
+              }
+              toast.success("Successfully saved!");
+            })
+            .catch((error) => toast.error("Could not be saved.")); // added 2
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestion</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster /> // Added 3
+    </>
+  );
+};
 ```
 
 ### -
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,6 +3,7 @@ import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const [users, setUsers] = useState<User[]>();
@@ -12,40 +13,51 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
       .get<User[]>("http://localhost:3000/api/users")
       .then((res) => {
         setUsers(res.data);
-        console.log(res.data);
+        // console.log(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || "unassigned"}
-      onValueChange={(userId) => {
-        const valueUserId = userId === "unassigned" ? null : userId;
-        fetch("http://localhost:3000/api/issues/" + issue.id, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            assignedToUserId: valueUserId,
-          }),
-        });
-      }}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestion</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || "unassigned"}
+        onValueChange={(userId) => {
+          const valueUserId = userId === "unassigned" ? null : userId;
+          fetch("/api/issues/" + issue.id, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              assignedToUserId: valueUserId,
+            }),
+          })
+            .then((res) => {
+              if (!res.ok) {
+                // if you use axios then you need to to do this only .catch() work directly
+                throw new Error("Network response was not ok");
+              }
+              toast.success("Successfully saved!");
+            })
+            .catch((error) => toast.error("Could not be saved."));
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestion</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
